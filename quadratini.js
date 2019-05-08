586ed7a1a3d80b91ac400d88710382f1fe7e055a
emanuele <emanuele.biotti@gmail.com>
2019-05-08 16:33:26 +0200

    aggiunta istruzioni esercizio

--- quadratini.js
+++ quadratini.js
@@ -1,11 +1,21 @@
-$('.diventaRosso').click(function() {
-  $(this).css({
-    'backgroundColor' : 'red'
-  });
-});
+// Creare una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+// Parte 2 (Bonus)
+// Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
-$('.diventaVerde').click(function() {
-  $(this).css({
-    'backgroundColor' : 'green'
-  });
-});
+//così funziona se assegno io inizialmente i colori ai quadratini
+// (cioé se assegnassi a 15 quadratini la classe "diventaRosso e a 49 la classe "diventaVerde)
+
+// $('.diventaRosso').click(function() {
+//   $(this).css({
+//     'background-color' : 'red'
+//   });
+// });
+//
+// $('.diventaVerde').click(function() {
+//   $(this).css({
+//     'background-color' : 'green'
+//   });
+// });
+
+// Tuttavia i quadratini devono avere a ogni refresh un'assegnazione random dei due colori!!
